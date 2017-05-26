74683d24a70d4805d78fc4e2cbc0733f558eb4e2
Vudong2804 <Vudong2804@gmail.com>
2017-05-26 11:13:11 -0700

    frmTimKiem

--- QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormTimDocGia_MuonTra.Designer.cs
+++ QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormTimDocGia_MuonTra.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.cmbDocGia = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnThoat);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnXem);
+            this.groupBox1.Controls.Add(this.cmbDocGia);
+            this.groupBox1.Location = new System.Drawing.Point(30, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(516, 140);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tìm thông tin";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(391, 45);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Quay lại";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Độc Giả ";
+            // 
+            // btnXem
+            // 
+            this.btnXem.Location = new System.Drawing.Point(273, 46);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(98, 21);
+            this.btnXem.TabIndex = 4;
+            this.btnXem.Text = "Xem Thông Tin";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // cmbDocGia
+            // 
+            this.cmbDocGia.FormattingEnabled = true;
+            this.cmbDocGia.Location = new System.Drawing.Point(93, 46);
+            this.cmbDocGia.Name = "cmbDocGia";
+            this.cmbDocGia.Size = new System.Drawing.Size(154, 21);
+            this.cmbDocGia.TabIndex = 3;
             // 
             // FormTimDocGia_MuonTra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 188);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FormTimDocGia_MuonTra";
             this.Text = "FormTimDocGia_MuonTra";
             this.Load += new System.EventHandler(this.FormTimDocGia_MuonTra_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.ComboBox cmbDocGia;
     }
 }
--- QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormTimDocGia_MuonTra.cs
+++ QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormTimDocGia_MuonTra.cs
@@ -22,7 +22,41 @@ namespace QuanLyThuVien_Nhom7
 
         private void FormTimDocGia_MuonTra_Load(object sender, EventArgs e)
         {
+            getdata();
         }
-        
+        public void getdata()
+        {
+            SqlConnection con = new SqlConnection(globalParameter.str);
+            SqlCommand cm = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable data = new DataTable();
+            cm.Connection = con;
+            cm.CommandText = "select * from DOCGIA";
+            da.SelectCommand = cm;
+            da.Fill(data);
+            cmbDocGia.DataSource = data;
+            cmbDocGia.DisplayMember = "MaHienThi";
+            cmbDocGia.ValueMember = "MaThe";
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            //FormMuonTra.docgia =
+            try
+            {
+                FormMuonTra mt = new FormMuonTra();
+                mt.docgia = int.Parse(cmbDocGia.SelectedValue.ToString());
+                this.Hide();
+                mt.ShowDialog();
+            }
+            catch { }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            FormMain mt = new FormMain();
+            this.Hide();
+            mt.ShowDialog();
+        }
     }
 }
