67d25022ebe87071486fc9b57cdab81ffed5dd63
manhhiep1996 <manhhiep1996@gmail.com>
2017-06-01 01:50:20 +0700

    [Hiep] form hướng dẫn

--- QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormHelp.cs
+++ QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormHelp.cs
@@ -21,5 +21,10 @@ namespace QuanLyThuVien_Nhom7
         {
 
         }
+
+        private void btnTrove_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
--- QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormHelp.designer.cs
+++ QuanLyThuVien22/QuanLyThuVien/QuanLyThuVien_Nhom7/FormHelp.designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHelp));
+            this.btnTrove = new System.Windows.Forms.Button();
+            this.rtbHelp = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // btnTrove
+            // 
+            this.btnTrove.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTrove.Location = new System.Drawing.Point(633, 473);
+            this.btnTrove.Name = "btnTrove";
+            this.btnTrove.Size = new System.Drawing.Size(75, 23);
+            this.btnTrove.TabIndex = 5;
+            this.btnTrove.Text = "Trở về";
+            this.btnTrove.UseVisualStyleBackColor = true;
+            this.btnTrove.Click += new System.EventHandler(this.btnTrove_Click);
+            // 
+            // rtbHelp
+            // 
+            this.rtbHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbHelp.BackColor = System.Drawing.SystemColors.Window;
+            this.rtbHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.rtbHelp.Location = new System.Drawing.Point(17, 18);
+            this.rtbHelp.Name = "rtbHelp";
+            this.rtbHelp.ReadOnly = true;
+            this.rtbHelp.Size = new System.Drawing.Size(725, 449);
+            this.rtbHelp.TabIndex = 4;
+            this.rtbHelp.Text = resources.GetString("rtbHelp.Text");
             // 
             // FormHelp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(759, 514);
+            this.Controls.Add(this.btnTrove);
+            this.Controls.Add(this.rtbHelp);
             this.Name = "FormHelp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormHelp";
@@ -44,5 +76,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTrove;
+        private System.Windows.Forms.RichTextBox rtbHelp;
     }
 }
